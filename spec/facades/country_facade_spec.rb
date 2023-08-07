75ab71ab52a2bd63c8a7adab7fc7ba6f63c0b4f1
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-07 09:14:24 -0600

    add facade specs.

---
+++ spec/facades/country_facade_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe CountryFacade do
+  describe "countries" do
+    it "gets all countries as objects" do
+      countries_list = CountryFacade.new.countries
+
+      expect(countries_list).to be_an Array
+
+      countries_list.each do |country|
+        expect(country).to be_a Country
+        expect(country.name).to be_a String
+      end
+    end
+  end
+end
