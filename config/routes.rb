e181c55e280afca65f8740f6e781c17ae8045420
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-09 02:56:33 -0600

    add "/favorites" to favorites#index route.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
       resources :learning_resources, only: %i[index]
       resources :users, only: %i[create]
       resources :sessions, only: %i[create]
-      resources :favorites, only: %i[create]
+      resources :favorites, only: %i[index create]
     end
   end
 end
