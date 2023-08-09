5fcb13b444e17d0dffa4c25518954a92b78d1fc2
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-08-08 21:32:22 -0600

    add sessions controller w/ #create.

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UserSerializer.new(user), status: 200
+    else
+      render json: {error:['Bad Credentials']}, status: 401
+    end
+  end
+end
